13b967d1421438bdde50631b3a2fb127d03baf93
quallsl <quallsl@me.com>
2026-05-01 01:14:25 -0700

    Fix submit route build error

--- app/api/submit-film/route.ts
+++ app/api/submit-film/route.ts
@@ -7,7 +7,7 @@ export async function POST(req: Request) {
     console.log("New Film Submission:", data);
 
     return NextResponse.json({ success: true });
-  } catch (error) {
+  } catch {
     return NextResponse.json(
       { success: false },
       { status: 500 }
