8aa2cbc6b4d8c34fd8ec3c4f6749b09e61445a8e
quallsl <quallsl@me.com>
2026-07-15 22:33:19 -0400

    Swap hero trailer from WonderBoy to AIV promo commercial

--- app/page.js
+++ app/page.js
@@ -284,7 +284,7 @@ export default function Page() {
         <AVODPlayer
           autoPlay
           src={getBunnyStreamUrl(
-            "https://player.mediadelivery.net/embed/697977/489bb454-5cb1-46ee-9353-09ebc899f726"
+            "https://player.mediadelivery.net/embed/697977/264c75e3-cf23-4154-a081-98883ca50742"
           )}
         />
       </div>
